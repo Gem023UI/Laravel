96ca9e8f8950aaff881f8020a40b30a03941f377
Jemuel Malaga <malagajemuel@gmail.com>
2025-02-12 11:02:47 +0800

    updated models directory

---
+++ myapp/app/Models/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $table = 'item';
+    protected $primaryKey = 'item_id';
+    public $timestamps = false;
+    protected $fillable = ['description', 'cost_price', 'sell_price'];
+   
+}
