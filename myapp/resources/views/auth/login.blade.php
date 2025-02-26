def47db9d932505d017a042abdc0db05a35209b3
Jemuel Malaga <malagajemuel@gmail.com>
2025-02-26 10:34:16 +0800

    updated views and resources

--- myapp/resources/views/auth/login.blade.php
+++ myapp/resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
+@section('body')
 <div class="container">
+   
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
